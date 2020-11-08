12f381451403ae6e1b33251f011f79659bcb05e4
NixGD <nixgdill@gmail.com>
2020-11-08 11:28:31 -0500

    simple button broadcasting

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,37 @@
 import React from 'react';
 
-const home = () => {
+const home = () =>  {
+  return (
+    <div>
+      <h1>Home</h1>
+      <ButtonListener/>
+    </div>
+  );
+}
+
+class ButtonListener extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {'isPressed': false};
+
+    this.bc = new BroadcastChannel('button');
+    this.bc.onmessage = this.buttonPressed.bind(this);
+
+  }
+
+  buttonPressed (){
+    this.setState({'isPressed': true})
+  }
+
+  render() {
     return (
-       <div>
-          <h1>Home</h1>
-           <p>Home page body content</p>
-       </div>
+      <p>
+        {this.state.isPressed? "Button pressed" : "Button not pressed"}
+      </p>
     );
+  }
+
 }
 
 export default home;
--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,12 +1,34 @@
 import React from 'react';
 
-const Practice = () => {
-    return (
-       <div>
-          <h1>Practice Page</h1>
-          <p>Practice page body content</p>
-       </div>
-    );
+const practice = () =>  {
+  return (
+    <div>
+      <h1>Practice Page</h1>
+      <BroadcastingButton />
+    </div>
+  );
 }
 
-export default Practice;
+class BroadcastingButton extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+    this.bc = new BroadcastChannel('button');
+  }
+
+  handleClick() {
+    this.bc.postMessage('Button pressed')
+  }
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        Press Button!
+      </button>
+    );
+  }
+}
+
+
+export default practice;
