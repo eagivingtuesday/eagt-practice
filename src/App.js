cfe17d647268ccf640a871db49c062b06fde906c
NixGD <nixgdill@gmail.com>
2020-11-08 10:50:29 -0500

    simple multi-page structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Home from './components/Home';
+import Practice from './components/Practice';
+
+class App extends Component {
+  render() {
+    return (
+       <BrowserRouter>
+        <div>
+          <Switch>
+             <Route path="/" component={Home} exact/>
+             <Route path="/practice" component={Practice}/>
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Practice.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Practice = () => {
+    return (
+       <div>
+          <h1>Practice Page</h1>
+          <p>Practice page body content</p>
+       </div>
+    );
+}
+
+export default Practice;
