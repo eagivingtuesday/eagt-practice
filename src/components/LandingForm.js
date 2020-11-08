64698299b1ccd97ac16916fa6a1710967182110f
Angelina Li <ali6@wellesley.edu>
2020-11-08 10:52:53 -0500

    added landing page

---
+++ src/components/LandingForm.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+class LandingForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      confirmDonation: false,
+      numPractice: 0
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('An essay was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Essay:
+          <textarea value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+    );
+  }
+}
