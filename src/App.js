7259550d304f6ff08da1d1f56915497400773b65
NixGD <nixgdill@gmail.com>
2020-11-08 12:04:33 -0500

    remove logo.svg import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-import logo from './logo.svg';
 import './App.css';
 
 import Home from './components/Home';
