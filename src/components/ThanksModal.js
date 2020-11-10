3571182e6082f4abd9827023be3031cf66e4afc8
Angelina Li <ali6@wellesley.edu>
2020-11-10 13:14:37 -0500

    tweaked UI of thanks modal

--- src/components/ThanksModal.js
+++ src/components/ThanksModal.js
@@ -1,17 +1,89 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faCheckCircle, 
+  faChevronRight,
+  faNewspaper,
+  faReceipt,
+  faShieldAlt,
+  faUserFriends
+} from '@fortawesome/free-solid-svg-icons';
+
 class ThanksModal extends React.Component {
+
+  renderActionRow(faIcon, header, caption) {
+    return (
+      <div className="row">
+        <div className="col-1 text-lightgrey">
+          <FontAwesomeIcon icon={faIcon} size="lg" />
+        </div>
+        <div className="col-10">
+          <b>{header}</b>
+          <p className="text-thanks-action text-muted">{caption}</p>
+        </div>
+        <div className="col-1 text-muted">
+          <FontAwesomeIcon icon={faChevronRight} />
+        </div>
+      </div>
+    );
+  }
+
   render() {
+    var header = (
+      <div className="row">
+        <div className="col-1"></div>
+        <div className="col-10"><h5>Facebook Pay</h5></div>
+        <div className="col-1">
+          <button type='button' class='close' data-dismiss='modal' aria-label='Close'>
+            <span aria-hidden='true'>&times;</span>
+          </button>
+        </div>
+      </div>
+    );
     return (
       <Modal.Dialog id="thanks-modal" className="shadow-lg" hidden={!this.props.show}>
-        <Modal.Header closeButton>
-          <Modal.Title>Facebook Pay</Modal.Title>
-        </Modal.Header>
 
         <Modal.Body>
-          <p>Thank you for your donation</p>
+          <div className="text-center">
+            {header}
+            <hr></hr>
+            <span className="text-success">
+              <FontAwesomeIcon id="thanks-check" icon={faCheckCircle} size="4x" />
+            </span>
+            <h5>Thank you for your donation</h5>
+            <p className="text-muted">
+              Thank you for your donation! A receipt was sent to yourEmail@gmail.com.
+            </p>
+          </div>
+            {this.renderActionRow(
+              faUserFriends,
+              "Invite Your Friends",
+              "Share this fundraiser with friends who you think would like to support this cause."
+            )}
+            {this.renderActionRow(
+              faNewspaper,
+              "Share in News Feed",
+              "Support this fundraiser by sharing in your News Feed to let friends know this cause is important to you."
+            )}
+            {this.renderActionRow(
+              faReceipt,
+              "See Receipt",
+              "View your receipt for this donation."
+            )}
+            {this.renderActionRow(
+              faShieldAlt,
+              "Add a PIN to protect your payments.",
+              "Once created, you'll enter your PIN as a security check on some payments."
+            )}
+          
         </Modal.Body>
+
+        <Modal.Footer>
+          <Button variant="primary" size="sm">Done</Button>
+        </Modal.Footer>
 
       </Modal.Dialog>
     );
