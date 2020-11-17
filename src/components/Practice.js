8b9ff2c6d999b6b94c8d453cfbb7ac54d78f7450
NixGD <nixgdill@gmail.com>
2020-11-17 10:47:58 -0500

    Combine confirm & practice confirm into one component

--- src/components/Practice.js
+++ src/components/Practice.js
@@ -1,35 +1,33 @@
 import React from 'react';
-import Page from './Page';
 import PracticeForm from './PracticeForm';
+import ConfirmForm from './ConfirmForm';
 import ThanksModal from './ThanksModal';
 
-class Practice extends Page {
+import { sendTime } from '../utils';
+
+class Practice extends React.Component{
   constructor(props) {
     super(props);
 
-    this.donationClick = this.donationClick.bind(this);
-    this.buttonChannel = new BroadcastChannel('button');
-
     this.state = {
-      title: "Practice Test",
+      showConfirm: false,
       donationMade: false,
-      allLoaded: false
+      allLoaded: false,
     };
-  }
 
-  donationClick(event) {
-    event.preventDefault();
-    if(!this.state.donationMade) {
-      this.buttonChannel.postMessage("pressed");
-      this.setState({
-        donationMade: true
-      });
-    }
+    this.donationClick = this.donationClick.bind(this);
+    this.submitDonation = this.submitDonation.bind(this);
+    this.hideConfirm = this.hideConfirm.bind(this);
+
+    this.loadedChannel = new BroadcastChannel('loaded');
+    this.loadedChannel.onmessage = this.loadedMessage.bind(this);
+
+    this.buttonChannel = new BroadcastChannel('button');
   }
 
   componentDidMount() {
-    this.loadedChannel = new BroadcastChannel('loaded');
-    this.loadedChannel.onmessage = this.loadedMessage.bind(this);
+
+    document.title = this.props.title;
   }
 
   loadedMessage(msg) {
@@ -40,13 +38,55 @@ class Practice extends Page {
     }
   }
 
+  donationClick(event) {
+    // Triggered when user clicks on the button at the bottom of the form
+    event.preventDefault();
+    if (!this.state.donationMade) {
+      if (this.props.useConfirm) {
+        this.setState({
+          showConfirm: true
+        });
+      } else {
+        this.submitDonation(event);
+      }
+    }
+  }
+
+  submitDonation(event) {
+    // Submit the final donation to the home page.  Render this page inactive.
+    event.preventDefault();
+    if (!this.state.donationMade) {
+      this.buttonChannel.postMessage("pressed");
+      this.setState({
+        showConfirm: false,
+        donationMade: true
+      });
+    }
+  }
+
+  hideConfirm(event) {
+    // Confirm form canceled
+    event.preventDefault();
+    if (!this.state.donationMade) {
+      this.setState({
+        showConfirm: false
+      });
+    }
+  }
+
   render() {
+    const buttonDisabled = this.state.showConfirm || this.state.donationMade
+
+    // Note: this.state.showConfirm is never true if ConfirmForm is not used.
     return (
       <div className="container" id="practice-container">
         <PracticeForm onClick={this.donationClick}
-                      disableBtn={this.state.donationMade}
+                      disableBtn={buttonDisabled}
                       hide={this.state.donationMade}
                       allLoaded={this.state.allLoaded} />
+        <ConfirmForm show={this.state.showConfirm}
+                       onClick={this.submitDonation}
+                       cancelClick={this.hideConfirm} />
         <ThanksModal show={this.state.donationMade} />
       </div>
     );
